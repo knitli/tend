56ff1a6ad8c54c397e4deb5f604185687587d6ae
Adam Poulemanos <adam@knitli.com>
2026-04-13 16:52:39 -0400

    fix: Remove unused ts-expect-error in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 /// <reference types="svelte" />
 import "./app.css";
 import { mount } from "svelte";
-// @ts-expect-error - No idea why it can't find the svelte types here, but it works fine
 import App from "./routes/+page.svelte";
 
 const appElement = document.getElementById("app");
